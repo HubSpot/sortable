2b92793cd7363f98f60986eae2ef59e6692b945b
Adam Schwartz <aschwartz@hubspot.com>
2013-11-19 22:43:21 -0500

    Remove forEach stuff

--- sorttable.js
+++ sorttable.js
@@ -103,7 +103,7 @@ sorttable = {
 
           // remove sorttable_sorted classes
           theadrow = this.parentNode;
-          forEach(theadrow.childNodes, function(cell) {
+          makeArray(theadrow.childNodes).forEach(function(cell) {
             if (cell.nodeType == 1) { // an element
               cell.className = cell.className.replace('sorttable_sorted_reverse','');
               cell.className = cell.className.replace('sorttable_sorted','');
@@ -393,57 +393,6 @@ fixEvent.stopPropagation = function() {
   this.cancelBubble = true;
 };
 
-// Dean's forEach: http://dean.edwards.name/base/forEach.js
-/*
-  forEach, version 1.0
-  Copyright 2006, Dean Edwards
-  License: http://www.opensource.org/licenses/mit-license.php
-*/
-
-// array-like enumeration
-if (!Array.forEach) { // mozilla already supports this
-  Array.forEach = function(array, block, context) {
-    for (var i = 0; i < array.length; i++) {
-      block.call(context, array[i], i, array);
-    }
-  };
-}
-
-// generic enumeration
-Function.prototype.forEach = function(object, block, context) {
-  for (var key in object) {
-    if (typeof this.prototype[key] == "undefined") {
-      block.call(context, object[key], key, object);
-    }
-  }
+var makeArray = function(arrayLikeThing) {
+  return Array.prototype.slice.call(arrayLikeThing);
 };
-
-// character enumeration
-String.forEach = function(string, block, context) {
-  Array.forEach(string.split(""), function(chr, index) {
-    block.call(context, chr, index, string);
-  });
-};
-
-// globally resolve forEach enumeration
-var forEach = function(object, block, context) {
-  if (object) {
-    var resolve = Object; // default
-    if (object instanceof Function) {
-      // functions have a "length" property
-      resolve = Function;
-    } else if (object.forEach instanceof Function) {
-      // the object implements a custom forEach method so use that
-      object.forEach(block, context);
-      return;
-    } else if (typeof object == "string") {
-      // the object is a string
-      resolve = String;
-    } else if (typeof object.length == "number") {
-      // the object is array-like
-      resolve = Array;
-    }
-    resolve.forEach(object, block, context);
-  }
-};
-
