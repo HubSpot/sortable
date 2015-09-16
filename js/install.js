474648ee9db924f68853652c3376ef882471fb86
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-09-15 23:44:09 -0400

    classlist fix

--- js/install.js
+++ js/install.js
@@ -3,6 +3,6 @@
 
   Array.prototype.forEach.call(document.querySelectorAll('table'), function(table){
     table.setAttribute('data-sortable', '');
-    table.add('sortable-theme-' + options.theme);
+    table.classList.add('sortable-theme-' + options.theme);
   });
 })();
