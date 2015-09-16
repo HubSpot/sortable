6977b950bc6fc840c55b301257ff84f26316d851
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-09-15 23:43:02 -0400

    setAttribute fix

--- js/install.js
+++ js/install.js
@@ -2,7 +2,7 @@
   var options = INSTALL_OPTIONS;
 
   Array.prototype.forEach.call(document.querySelectorAll('table'), function(table){
-    table.setAttribute('data-sortable');
+    table.setAttribute('data-sortable', '');
     table.add('sortable-theme-' + options.theme);
   });
 })();
